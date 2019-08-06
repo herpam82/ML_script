c340386d5cdf4dbefcb619acfcf1909563709c49
herpam82 <53352578+herpam82@users.noreply.github.com>
2019-08-05 22:35:34 -0300

    Add files via upload

--- list_products.sh
+++ list_products.sh
@@ -1,11 +1,15 @@
-​#!/bin/bash
+#!/bin/bash
 
 LOG="/tmp/log"
 
-seller_ids=(81644614)
 site_id=MLA
+seller_ids=81644614
+
+echo "Begginning LOG on $date" > $LOG
 
 for seller_id in ${seller_ids[*]}
 do
+	echo "seller_id=${seller_id}" >> $LOG
+	echo "-------------------" >> $LOG
 	curl -s https://api.mercadolibre.com/sites/${site_id}/search?seller_id=${seller_id} | ./jq '.results[] | { id: .id, title: .title, category_id: .category_id }' >> $LOG
 done
