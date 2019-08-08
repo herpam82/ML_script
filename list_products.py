7fa6ac21b601ab23395ed3ab5df2e441f5e95db4
herpam82 <53352578+herpam82@users.noreply.github.com>
2019-08-08 17:34:24 -0300

    Add files via upload
    Se agregó que guarde los datos en el log

--- list_products.py
+++ list_products.py
@@ -3,22 +3,24 @@ import json, urllib.request
 site_id = 'MLA'
 seller_id = ["81644614"]
 
+log_file = open("/tmp/LOG","w+")
+
 for id in seller_id:
 
     url = "https://api.mercadolibre.com/sites/"+site_id+"/search?seller_id="+id
-
-    print(url)
 
     json_url = urllib.request.urlopen(url)
 
     data = json.loads(json_url.read())
 
-
     for x in data['results']:
-        print("id:", x['id'],"\n","title:", x['title'],"\n","category_id:", x['category_id'])
+        L = ["id:", x['id'],"\n","title:", x['title'],"\n","category_id:", x['category_id']]
+        log_file.writelines(L)
         url2 = "https://api.mercadolibre.com/categories/"+x['category_id']
         json_url2 = urllib.request.urlopen(url2)
         data2 = json.loads(json_url2.read())
         if x['category_id'] == data2['id']:
-            print(" categoria:",data2['name'],"\n")
+            L2 = ["\n","categoria:",data2['name'],"\n\n"]
+            log_file.writelines(L2)
+log_file.close()
 
